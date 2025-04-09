9b6d38be8f1f40eb4f95bc906c246fd7472c79c6
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-04-09 10:18:14 -0400

    Clean up array and logic of script

--- script.rb
+++ script.rb
@@ -1,36 +1,18 @@
 # Convert all letters to a numeric value
-a = 1
-b = 2
-c = 3
-d = 4
-e = 5
-f = 6
-g = 7
-h = 8
-i = 9
-j = 10
-k = 11
-l = 12
-m = 13
-n = 14
-o = 15
-p = 16
-q = 17
-r = 18
-s = 19
-t = 20
-u = 21
-v = 22
-w = 23
-x = 24
-y = 25
-z = 26
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+            "n","o","p","q","r","s","t","u","v","w","x","y","z"]
 # prompt the user to enter a string and shift value
 def caeser_cipher(string,shift)
 # convert string to an array of single letters/spaces
-  array = string.spit("")
-# map the array so each letter string is now an integer
-# This should allow you to reference the value and build an array by that value
+  array = string.split("")
+  index_value = alphabet.index(array[0])
+# Iterate over the string array to find the index value in the alphabet array
+# Take the shift amount and add it to the index value to find the new value
+# Three Scenarios:
+# 1) index_value + shift is >= 0 and <= 26.  Take value and find new value
+# 2) index_value + shift is > 26.  Loop back around by calculating the remainder of 
+#    index_value + shift % 26
+# 3) index_value + shift is < 0.  Loop around so shift_value = 26 - (index_value+shift*-1)
 # apply the shift to the numerical values
 # convert back to string
 # return new string
