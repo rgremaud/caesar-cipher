40cc89d7ed2ff3f65ae76a5369e198ff53424524
Romain Gremaud <rgremau@gmail.com>
2025-04-09 13:55:14 -0400

    Logic works for a single word
    Script works on single words w/no spaces/punctutation.  Upcase and
    downcase needs some work.

--- script.rb
+++ script.rb
@@ -1,19 +1,56 @@
 # Convert all letters to a numeric value
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
-            "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+
+
 # prompt the user to enter a string and shift value
 def caeser_cipher(string,shift)
-# convert string to an array of single letters/spaces
+
+  alphabet_downcase = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+  "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+  alphabet_upcase = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+  "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
   array = string.split("")
-  index_value = alphabet.index(array[0])
-# Iterate over the string array to find the index value in the alphabet array
-# Take the shift amount and add it to the index value to find the new value
-# Three Scenarios:
-# 1) index_value + shift is >= 0 and <= 26.  Take value and find new value
-# 2) index_value + shift is > 26.  Loop back around by calculating the remainder of 
-#    index_value + shift % 26
-# 3) index_value + shift is < 0.  Loop around so shift_value = 26 - (index_value+shift*-1)
-# apply the shift to the numerical values
-# convert back to string
-# return new string
+
+  p array
+
+  number_array = array.map do |letter|
+    if letter.upcase == letter
+      alphabet_upcase.index(letter)
+    else
+      alphabet_downcase.index(letter)
+    end
+  end
+
+  p number_array
+
+  shift_array = number_array.map do |number|
+    if number + shift >= 0 && number + shift <= 25
+      number = number + shift
+    elsif number + shift > 25
+      number = (number + shift) % 25
+    elsif number + shift < 0
+      number = 25 - number
+    else
+    end
+  end
+
+  p shift_array
+
+  # map shift_array to cipher_array
+  cipher_array = shift_array.map do |number|
+  # if index array == upcase
+    if array[shift_array.index(number)] == array[shift_array.index(number)].upcase
+      number = alphabet_upcase[number]
+    elsif array[shift_array.index(number)] != array[shift_array.index(number)].upcase
+      number = alphabet_downcase[number]
+    end
+  end
+  p cipher_array
 end
+
+caeser_cipher("TEst",3)
+
+caeser_cipher("WHvw",22)
