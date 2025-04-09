0e567ad21f57dba4f5b8b644ee8fcbf721e7879c
Romain Gremaud <rgremau@gmail.com>
2025-04-08 21:04:41 -0400

    Add initial framework for script

---
+++ script.rb
@@ -0,0 +1,36 @@
+# Convert all letters to a numeric value
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+k = 11
+l = 12
+m = 13
+n = 14
+o = 15
+p = 16
+q = 17
+r = 18
+s = 19
+t = 20
+u = 21
+v = 22
+w = 23
+x = 24
+y = 25
+z = 26
+# prompt the user to enter a string and shift value
+def caeser_cipher(string,shift)
+# convert string to an array of single letters/spaces
+  array = string.spit("")
+# map the string to the corresponding numerical values
+# apply the shift to the numerical values
+# convert back to string
+# return new string
+end
